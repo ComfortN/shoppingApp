81625d5b9de75ebf6fca103a61c30724a8489f9e
Comfort Ngwenya <110750853+ComfortN@users.noreply.github.com>
2026-03-17 09:31:03 +0200

    Update background image in Landing component

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Landing() {
   return (
     <div className="h-screen bg-cover bg-center flex flex-col justify-center items-center text-white"
-     style={{ backgroundImage: "url('/img/bg.jpg')" }}>
+     style={{ backgroundImage: "url('https://i.pinimg.com/736x/1f/7d/f9/1f7df98a2047c3f243d428e6322e5341.jpg')" }}>
 
       <h1 className="text-5xl font-bold mb-4">GreenLeaf Co.</h1>
 
